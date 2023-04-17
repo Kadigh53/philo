fc8b71061746ab3caac11819016c58d4ab4d453f
k16aO2digOOh <abdlkadraoutem@gmail.com>
2023-04-17 05:20:15 +0000

    czx

--- main.c
+++ main.c
@@ -3,14 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaoutem- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 21:50:25 by aaoutem-          #+#    #+#             */
-/*   Updated: 2023/04/16 21:51:09 by aaoutem-         ###   ########.fr       */
+/*   Updated: 2023/04/17 05:02:49 by aaoutem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
 
-int	main()
+int i=0;
+void	test()
 {
+	i++;
+	sleep(3);
+	printf("wali iewaren %d \n",i);
+	sleep(2);
+	printf("\n");
+	printf("wali iewaren %d \n",i);
+}
+void	test2()
+{
+	sleep(3);
+	printf("wissin ayd  %d \n",i);
+	sleep(1);
+	i--;
+	printf("wissin ayd2 %d \n",i);
+}
+
+int	main(int ac, char **av)
+{
+	pthread_t	t1;
+	pthread_t	t2;
+	pthread_create(&t1,NULL,&test,NULL);
+	pthread_create(&t2,NULL,&test2,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	return (0);
 }
